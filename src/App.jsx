bbe134b4a656216dba2a4bbc5e2c57573839a424
Odilbek-Arziev <MasterM111nd@mail.ru>
2024-08-12 21:08:17 +0300

    Added a component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Worm from './components/worm.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +19,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Worm />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/worm.jsx
@@ -0,0 +1,10 @@
+function Worm() {
+    let result = [];
+    for (let i = 0; i < 5; i++) {
+        result.push(<button>worm</button>);
+    }
+    return (result); 
+    //return (<h1>kuku</h1>); 
+}
+
+export default Worm;
